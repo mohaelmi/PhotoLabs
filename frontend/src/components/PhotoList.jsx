e53c9187d239279a7c34ba397066b9f5d9c43b65
mohaelmi <moha.abdi1771@gmail.com>
2023-09-12 18:38:15 -0500

    PhotoList Component

--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import "../styles/PhotoList.scss";
+import PhotoListItem from "./PhotoListItem";
 
 const sampleDataForPhotoList = [
   {
@@ -57,9 +58,14 @@ const sampleDataForPhotoList = [
 ];
 
 const PhotoList = () => {
+
+  
+  const renderPhotos = () => {
+    return sampleDataForPhotoList.map((photo, index) => <PhotoListItem photo = {photo} key={photo.id}/>)
+   }
   return (
     <ul className="photo-list">
-      {/* Insert React */}
+       { renderPhotos() }
     </ul>
   );
 };
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -11,12 +11,12 @@ const PhotoListItem = (props) => {
   return (
     <article className="photo-list__item">
       <PhotoFavButton />
-      <img src={props.photoListItemData.imageSource} className="photo-list__image" />
+      <img src={props.photo.urls.full} className="photo-list__image" />
       <div className="photo-list__user-details">
-        <img src={props.photoListItemData.profile}  className="photo-list__user-profile"/>
+        <img src={props.photo.user.profile}  className="photo-list__user-profile"/>
         <div >
-          <label className="photo-list__user-info">{ props.photoListItemData.username }</label><br/>
-          <label className="photo-list__user-location photo-list__user-info">{props.photoListItemData.location.city}, {props.photoListItemData.location.country}</label>
+          <label className="photo-list__user-info">{ props.photo.user.username }</label><br/>
+          <label className="photo-list__user-location photo-list__user-info">{props.photo.location.city}, {props.photo.location.country}</label>
         </div>
       </div>
     </article>
