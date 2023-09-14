d76fa1267314444fb0868531e1450051226e27db
mohaelmi <moha.abdi1771@gmail.com>
2023-09-13 19:40:06 -0500

    notifications for liked photos

--- frontend/src/components/FavBadge.jsx
+++ frontend/src/components/FavBadge.jsx
@@ -3,11 +3,10 @@ import FavIcon from './FavIcon';
 
 import '../styles/FavBadge.scss';
 
-const FavBadge = ({ isFavPhotoExist }) => {
-  console.log(!!isFavPhotoExist);
+const FavBadge = ({ isFavPhotoExist, selected }) => {
   return (
-    <div className='fav-badge'>
-      <FavIcon displayAlert={!!isFavPhotoExist}/>
+    <div className="fav-badge">
+      <FavIcon displayAlert={!!isFavPhotoExist} selected = {selected}  />
     </div>
   ) 
 };
